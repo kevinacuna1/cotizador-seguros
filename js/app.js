fb9f3f3aeb6d9a9e39f04000a427bb002554e4f1
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-10-21 21:02:28 -0300

    Validar el formulario

--- js/app.js
+++ js/app.js
@@ -27,3 +27,30 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con los años
 });
+
+eventListeners();
+
+function eventListeners() {
+    const form = document.querySelector('#cotizar-seguro');
+    form.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+
+    // Leer el año seleccionado
+    const year = document.querySelector('#year').value;
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if (marca === '' || year === '' || tipo === '') {
+        console.log('Faltan datos');
+        return;
+    } else {
+        console.log('Cotizando...');
+    }
+}
